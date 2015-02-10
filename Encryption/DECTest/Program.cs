6a11808f6eb74deb54c27b4895b598082ae006e5
zcs0228 <zcs0228@yahoo.com>
2015-02-10 12:14:31 +0800

    start

---
+++ Encryption/DECTest/Program.cs
@@ -0,0 +1,20 @@
+﻿using Encryption.DESFile;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DECTest
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string fileIn = @"c:\AccountBox";
+            string fileOut = @"c:\AccountBox.bat";
+            DESFileEncryption.EncryptFile(fileIn, fileOut, "zcs&wxt00896014");
+            //DESFileEncryption.DecryptFile(fileOut, fileIn, "zcs&wxt");
+        }
+    }
+}
